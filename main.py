495358d9ddc6651d889ca92346999a63b0517d10
zackaids <zalkar.ai77@gmail.com>
2025-04-03 21:07:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route('/')
 def home():
@@ -369,5 +369,7 @@ def home():
 #     return jsonify(stats)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    
